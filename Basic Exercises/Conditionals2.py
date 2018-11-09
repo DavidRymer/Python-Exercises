a9c2613afa9dffc079e40088ad4d2f540799f387
David Rymer <david-rymer@hotmail.co.uk>
2018-11-09 14:10:26 +0000

    Lists

--- Basic Exercises/Conditionals2.py
+++ Basic Exercises/Conditionals2.py
@@ -7,12 +7,7 @@ def add(num1, num2, modifier):
         else:
             return num1 + num2
     else:
-        if num1 == 0:
-            return num2
-        elif num2 == 0:
-            return num1
-        else:
-            return num1 * num2
+        return num1 * num2
 
 
 
---
+++ Basic Exercises/Lists.py
@@ -0,0 +1,7 @@
+from Conditionals2 import add
+
+a = [3, 4, 25, 6, 8, 4]
+
+for x in a:
+    print(add(x, x, True))
+
