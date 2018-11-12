533c327d185c29c1dd45a23507e79e9b2d5a1da1
David Rymer <david-rymer@hotmail.co.uk>
2018-11-12 11:40:01 +0000

    Naming convention correction for "Too Hot?"

--- Intermediate Exercises/TooHot.py
+++ Intermediate Exercises/TooHot.py
@@ -1,7 +1,7 @@
-def too_hot(temperature, isSummer):
-    if isSummer and 60 < temperature < 100:
+def too_hot(temperature, is_summer):
+    if is_summer and 60 < temperature < 100:
         return True
-    elif not isSummer and 60 < temperature < 90:
+    elif not is_summer and 60 < temperature < 90:
         return True
     else:
         return False
