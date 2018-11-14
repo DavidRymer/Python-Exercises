4a180aa3e1b441ddbf6c5dc6d4c3c2ee5720cd28
David Rymer <david-rymer@hotmail.co.uk>
2018-11-14 13:11:05 +0000

    Working with files

---
+++ IntermediateExercises/WorkingWithFiles/Person.py
@@ -0,0 +1,26 @@
+class Person(object):
+    def __init__(self, name, occupation, age):
+        self.name = name
+        self.occupation = occupation
+        self.age = age
+
+
+people = [
+    Person("David Rymer", "Software Developer", 21),
+    Person("Donny Soldier", "Joiner", 32),
+    Person("Donald Trump", "President", 72),
+    Person("John Smith", "Plumber", 33),
+    Person("Paul Hawkins", "Chemist", 45)
+]
+
+file = open("test.txt", "w")
+
+for person in people:
+    file.write("Name: " + person.name + "\nOccupation: " + person.occupation + "\nAge: " + str(person.age) + "\n \n")
+
+file.close()
+
+with open("test.txt", "a") as myfile:
+    for person in people:
+        myfile.write(
+            "Name: " + person.name + "\nOccupation: " + person.occupation + "\nAge: " + str(person.age) + "\n \n")
